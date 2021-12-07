9142bc8fee95607cb5517decfb0b86572b5fdf6c
James Kulu <jameskulu55@gmail.com>
2021-12-07 22:11:21 +0545

    Initial Commit

---
+++ Accounts/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+ROLE_CHOICES = (
+    ("user", "user"),
+    ("admin", "admin"),
+)
+
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    firstName = models.CharField(max_length=150, blank=True, null=True)
+    lastName = models.CharField(max_length=150, blank=True, null=True)
+    about = models.TextField(max_length=500, blank=True, null=True)
+    role = models.CharField(max_length=5, choices=ROLE_CHOICES, default="user")
+    isActive = models.BooleanField(default=True)
+    isVerified = models.BooleanField(default=False)
+    emailToken = models.CharField(max_length=150, blank=True, null=True)
+    emailTokenExpireDate = models.DateTimeField(blank=True, null=True)
+    resetToken = models.CharField(max_length=150, blank=True, null=True)
+    resetTokenExpireDate = models.DateTimeField(blank=True, null=True)
+    createdAt = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updatedAt = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    def __str__(self):
+        return self.email
